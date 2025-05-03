c335b244ab6d49e4b4e8515a9eec3d3c40a7a765
nivin shabat <nivin20012001@gmail.com>
2025-05-03 15:08:09 +0300

    Migration and Realtion Model

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+    public function category()
+    {
+        return $this->belongsTo('Category::class','Category_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo('User::class','user_id');
+    }
+    public function comments()
+    {
+        return $this->hasMany('Comment::class','post_id');
+    }
+    public function images()
+    {
+        return $this->hasMany('Image::class','post_id');
+    }
+}
---
+++ database/migrations/2025_04_28_040735_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('desc');
+            $table->boolean('comment_able')->default(1);
+            $table->boolean('status');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
